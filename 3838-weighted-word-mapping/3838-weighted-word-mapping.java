993c4167ec64f678a392b011a5ee7da725f33602
Sahil Sharma <officialsahil548k@gmail.com>
2026-06-14 00:02:03 +0530

    Time: 2 ms (96.85%), Space: 46.7 MB (33.48%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,27 +1,18 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        char[] letters = {'a', 'b', 'c', 'd', 
-        'e','f', 'g', 'h', 'i', 'j', 
-        'k', 'l', 'm', 'n', 'o', 'p', 
-        'q','r', 's', 't', 'u', 'v', 
-        'w','x', 'y', 'z'};
-        int n = letters.length;
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Character> map2 = new HashMap<>();
 
-        for(int i=0; i<26; i++){
-            map1.put(letters[i],weights[i]);
-            map2.put(i,letters[n-1-i]);
-        }
-        
         StringBuilder ans = new StringBuilder();
-        for(String word : words){
-            int result = 0;
-            for(int i=0; i<word.length(); i++){
-                result += map1.get(word.charAt(i));
+
+        for (String word : words) {
+            int sum = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                sum += weights[word.charAt(i) - 'a'];
             }
-            ans.append(map2.get(result%26));;
+            sum = sum%26;
+            ans.append((char)('z' - sum));
         }
+
         return ans.toString();
     }
 }
